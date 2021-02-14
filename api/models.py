ff013c680c3c881387493e43402e73ab9ad0d432
onikazakino <kazu09201107@gmail.com>
2021-02-14 16:52:03 +0900

    create_repository

---
+++ api/models.py
@@ -0,0 +1,85 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.conf import settings
+
+def upload_avatar_path(instance, filename):
+    ext = filename.split('.')[-1]
+    return '/'.join(['avaters', str(instance.userProfile.id)+str(instance.nickName)+str(".")+str(ext)])
+
+def upload_post_path(instance, filename):
+    ext = filename.split('.')[-1]
+    return '/'.join(['posts', str(instance.userProfile.id)+str(instance.title)+str(".")+str(ext)])
+
+class UserManager(BaseUserManager):
+    #通常ユーザ用
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError("email is must")
+
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    #管理者用
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(max_length=50, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.email
+
+class Profile(models.Model):
+    nickName = models.CharField(max_length=20)
+    userProfile = models.OneToOneField(
+        settings.AUTH_USER_MODEL, related_name='userProfile',
+        on_delete=models.CASCADE
+    )
+
+    createed_on = models.DateTimeField(auto_now_add=True)
+    img = models.ImageField(blank=True, null=True, upload_to=upload_avatar_path)
+
+    def __str__(self):
+        return self.nickName
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    userPost = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name='userpost',
+        on_delete=models.CASCADE
+    )
+
+    createed_on = models.DateTimeField(auto_now_add=True)
+    img = models.ImageField(blank=True, null=True, upload_to=upload_post_path)
+    liked = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='liked', blank=True)
+
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+    text = models.CharField(max_length=100)
+    userComment = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name='userComment',
+        on_delete=models.CASCADE
+    )
+
+post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+def __str__(self):
+    return self.text
+
